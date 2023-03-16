4b9cd8aaa6609164d299a5c88b9d0662e64e0789
bojidarts <tsachev2@gmail.com>
2023-03-16 13:52:03 +0200

    Problem Nr6

--- src/TryingExam_2_2/Task_06.java
+++ src/TryingExam_2_2/Task_06.java
@@ -6,7 +6,39 @@ public class Task_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int countClient = Integer.parseInt(scanner.nextLine());
+        double totalPrice = 0;
 
 
+        for (int i = 1; i <= countClient; i++) {
+            double pricePerClient = 0;
+            int counter = 0;
+            String input = scanner.nextLine();
+            while (!input.equals("Finish")) {
+                switch (input) {
+                    case "basket":
+                        pricePerClient += 1.50;
+                        counter++;
+                        break;
+                    case "wreath":
+                        pricePerClient += 3.80;
+                        counter++;
+                        break;
+                    case "chocolate bunny":
+                        pricePerClient += 7;
+                        counter++;
+                        break;
+                }
+
+
+                input = scanner.nextLine();
+            }
+            if (counter % 2 == 0){
+                pricePerClient = pricePerClient - pricePerClient * 0.20;
+            }
+            totalPrice += pricePerClient;
+            System.out.printf("You purchased %d items for %.2f leva.%n", counter, pricePerClient);
+        }
+        System.out.printf("Average bill per client is: %.2f leva.", totalPrice / countClient);
     }
 }
